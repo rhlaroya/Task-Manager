55cf047c9a54b7767db6b6f27f2b986d5c0cf48c
Regae Laroya <regae.laroya@collabera.com>
2022-01-19 14:44:15 -0500

    Added creation and reading of users and tasks

---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value){
+            if(value.length < 6){
+                throw new Error('Password must be at least 6 characters long')
+            } else if(value.includes('password')){ 
+                throw new Error('Please create a more secure password')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    }
+})
+
+module.exports = User
