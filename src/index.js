021c34e1f992c5c8f046364a1e33944001a5e2fb
Regae Laroya <regae.laroya@collabera.com>
2022-01-20 16:04:00 -0500

    Implemented jsonwebtokens

--- src/index.js
+++ src/index.js
@@ -6,6 +6,20 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     if(req.method === 'GET') {
+//         res.send('GET requests are disabled')
+//     } else {
+//         next()
+//     }
+// })
+
+app.use((req, res, next) => {
+    res.status(503).send('Site is currently down try back soon')
+})
+
+//503 under m t b s
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -14,17 +28,14 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const myFunction = async () =>  {
-    const password = 'ScrawnyGirl0'
-    const hashedPassword = await bcrypt.hash(password, 8)
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days'})
+    console.log(token)
 
-    console.log(password)
-    console.log(hashedPassword)
-
-    const isMatch = await bcrypt.compare('ScrawnyGirl0', hashedPassword)
-    console.log(isMatch)
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
 }
 
 myFunction()
